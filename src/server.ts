07ec69a59225f232788ede90c76a19c303d92c0f
DVinciusBonin <vinibonin007@gmail.com>
2021-06-26 01:07:56 -0300

    enable cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import 'reflect-metadata'
 import express, { Request, Response, NextFunction, response } from 'express'
 import 'express-async-errors'
 import './database'
+import cors from 'cors';
 import { router } from './routes'
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(router)
